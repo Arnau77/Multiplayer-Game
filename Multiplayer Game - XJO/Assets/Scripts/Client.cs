d1820ebfe3382b6b5bdac19b31b6c3497d7d5f6b
Yuye <47743107+Rugiacreed@users.noreply.github.com>
2021-12-14 14:40:34 +0100

    Created scripts client-server

---
+++ Multiplayer Game - XJO/Assets/Scripts/Client.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net.Sockets;
+using System.Net;
+using System;
+using System.IO;
+
+public class Client : MonoBehaviour {
+    private bool socketReady = false;
+    private TcpClient socket;
+    private NetworkStream stream;
+    private StreamWriter writer;
+    private StreamReader reader;
+
+    public void OnConnectedToServer()
+    {
+        
+    }
+
+}
