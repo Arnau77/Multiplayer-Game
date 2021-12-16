4510a97c1d03af9afeeff226de447e51eaff05a9
Arnau77 <47742576+Arnau77@users.noreply.github.com>
2021-12-16 09:50:59 +0100

    base of upd connection partially done

--- Multiplayer Game - XJO/Assets/Scripts/NewClient.cs
+++ Multiplayer Game - XJO/Assets/Scripts/NewClient.cs
@@ -2,70 +2,110 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Net;
 using System.Net.Sockets;
 using UnityEngine;
+using System.Text;
+using System.Threading;
 using UnityEngine.UI;
 
 public class NewClient : MonoBehaviour
 {
-
-    private bool socketReady;
-    private TcpClient socket;
-    private NetworkStream stream;
+    private IPEndPoint ipDestination;
+    private EndPoint serverPoint;
+    private Socket socket;
+    private List<string> textsToSend = new List<string>();
+    private List<Action> actions = new List<Action>();
+    private object actionLock;
+    private object textLock;
+    private Thread clientListenThread;
+    private Thread clientSendThread;
     private StreamWriter writter;
     private StreamReader reader;
 
+    void Start()
+    {
+        actionLock = new object();
+        textLock = new object();
+    }
     public void ConnectToServer()
     {
-        if (socketReady) //if connected, ignore function
-            return;
+        socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        ipDestination = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 6162);
+        serverPoint = new IPEndPoint(IPAddress.Any, 0);
+        clientListenThread = new Thread(ClientListenThread);
+        clientSendThread = new Thread(ClientSendThread);
 
-        string host = "127.0.0.1";
-        int port = 6163;
-
-        string h;
-        int p;
-
-        //let the player decide the server and port to connect, if not we keep the default values
-       //h = GameObject.Find("HostInput").GetComponent<InputField>().text;
-       //if (h != "")
-       //    host = h;
-       //int.TryParse(GameObject.Find("PortInput").GetComponent<InputField>().text, out p);
-       //if (p != 0)
-       //    port = p;
-
-        try //we create the socket for connection
-        {
-            socket = new TcpClient(host, port);
-            stream = socket.GetStream();
-            writter = new StreamWriter(stream);
-            reader = new StreamReader(stream);
-
-        }
-        catch(Exception e)
-        {
-            Debug.Log("Socket Error : " + e.Message);
-        }
+        clientListenThread.Start();
+        clientSendThread.Start();
     }
 
     private void Update()
     {
-        if (socketReady)
+        lock (actionLock)
         {
-            if (stream.DataAvailable)
+            while (actions.Count > 0)
             {
-                string data = reader.ReadLine();
-                if (data != null)
-                    OnIncomingData(data);
-            
-
+                Action action = actions[0];
+                actions.RemoveAt(0);
+                action();
             }
         }
+        //if (socketReady)
+        //{
+        //    if (stream.DataAvailable)
+        //    {
+        //        string data = reader.ReadLine();
+        //        if (data != null)
+        //            OnIncomingData(data);
+
+
+        //    }
+        //}
+    }
+
+    void ClientSendThread()
+    {
+        byte[] buffer;
+        List<string> localTexts;
+       
+        while (true)
+        {
+            lock (textLock)
+            {
+                localTexts = textsToSend;
+                textsToSend.Clear();
+            }
+            for (int i = 0; i < localTexts.Count; i++)
+            {
+                buffer = Encoding.ASCII.GetBytes(localTexts[i]);
+                socket.SendTo(buffer, buffer.Length, SocketFlags.None, ipDestination);
+
+            }
+
+
+        }
+    }
+
+    void ClientListenThread()
+    {
+        byte[] buffer = new byte[100];
+
+        socket.ReceiveFrom(buffer, ref serverPoint);
+
+        Debug.Log(Encoding.ASCII.GetString(buffer));
     }
 
     private void OnIncomingData(string data)
     {
         Debug.Log("Server : " + data);
+    }
+
+    private void OnDestroy()
+    {
+        socket.Close();
+        clientSendThread.Abort();
+        clientListenThread.Abort();
     }
 
 }
